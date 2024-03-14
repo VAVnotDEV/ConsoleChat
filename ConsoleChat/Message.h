edfb6ece1b948cb7d9528dfc7b2edf9166e7ad0f
VAV <veselyi93rus@gmail.com>
2024-03-14 22:31:48 +0500

    Add Message class

--- ConsoleChat/Message.h
+++ ConsoleChat/Message.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+#include "User.h"
+
+using namespace std;
+
+template <class T>
+class Message
+{
+private:	
+	string _messageTo;
+	string _messageForm;
+	T _message;
+
+
+};
